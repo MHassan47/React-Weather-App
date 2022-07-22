4ea183af2a0a655917f263328c1cd869fee17809
Mohamed <mhassankd35@gmail.com>
2022-07-22 00:28:30 -0500

    refactors weather info to be dynamic not hardcoded

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import axios from "axios";
 import { FaSearch } from "react-icons/fa";
 function App() {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState();
+  const [location, setLocation] = useState("");
+  const key = process.env.REACT_APP_API_KEY;
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
+
   const searchLocation = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === "Enter" || event.onClick) {
       axios.get(url).then((response) => {
         setData(response.data);
+        console.log(response);
       });
     }
   };
-
+  console.log(location);
+  console.log(data);
   return (
     <div className="app">
       <div className="container">
@@ -28,26 +32,26 @@ function App() {
             placeholder="Enter a Location"
           />
           <div className="search_icon">
-            <FaSearch onClick />
+            <FaSearch onClick={searchLocation} />
           </div>
         </div>
         <div className="header">
-          <div className="location">Toronto</div>
+          <div className="location">{data.name}</div>
           <div className="temperature">
-            <h1>24°C</h1>
+            <h1>{data.main.temp}°C</h1>
           </div>
         </div>
         <div className="weather_info">
           <div className="feels_like">
-            <p>20°C</p>
+            <p>{data.main.feels_like}</p>
             <p>Feels like</p>
           </div>
           <div className="humidity">
-            <p>20%</p>
+            <p>{data.main.humidity}%</p>
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p>15KPH</p>
+            <p>{data.wind.speed}MPH</p>
             <p>Wind Speed</p>
           </div>
         </div>
