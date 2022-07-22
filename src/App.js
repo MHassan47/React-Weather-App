06b91e18bfc14c039eda379194c223afc2f62124
Mohamed <mhassankd35@gmail.com>
2022-07-21 21:55:09 -0500

    adds hardcoded weather data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import "./index";
+import axios from "axios";
 
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.API_KEY}`;
 function App() {
-  return <div className="App">Hello World</div>;
+  const [location, setLocation] = useState();
+
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="header">
+          <div className="location">Toronto</div>
+          <div className="temperature">
+            <p>24°C</p>
+          </div>
+          <div className="humidity">20%</div>
+          <div className="wind">15KPH</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
